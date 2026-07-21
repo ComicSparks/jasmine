2f780551e8c2dc6dcdf66a7bf4b7e54a4c2c40e5
niuhuan <20847533+niuhuan@users.noreply.github.com>
2026-07-21 10:23:34 +0800

    :recycle: fix github actions runner

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -14,5 +14,9 @@ class AppDelegate: FlutterAppDelegate {
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
+
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
     
 }
